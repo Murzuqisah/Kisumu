8c8633e3150ddedd3d3b7783468599e2dc5fe6d4
jamos <amosjoel91@gmail.com>
2024-09-06 12:21:59 +0300

    feat: add lexer and tokenization functions add tests for functions

--- pkg/parser/lexer.go
+++ pkg/parser/lexer.go
@@ -1,1 +1,551 @@
 package parser
+
+import (
+	"fmt"
+)
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+type Lexer struct { // position and readPosition are used to access characters in input(as index)
+	input        string
+	position     int  // index of the starting position of the current token(the previous character)
+	readPosition int  // index of the current character
+	currentChar  rune // current character under examination
+}
+
+const (
+	ILLEGAL TokenType = "ILLEGAL" // Invalid token/unknown character
+	EOF     TokenType = "EOF"     // End of file
+
+	// Identifiers and literals
+	INT        TokenType = "INT"        // 1323145567890
+	STRING     TokenType = "STRING"     // concatenate, slice, and get
+	IDENTIFIER TokenType = "IDENTIFIER" // variable name, function name, or struct name
+
+	// Operators and delimiters
+	OPEN_BRACKET      TokenType = "OPEN_BRACKET"      // [
+	CLOSE_BRACKET     TokenType = "CLOSE_BRACKET"     // ]
+	OPEN_CURLY        TokenType = "OPEN_CURLY"        // {
+	CLOSE_CURLY       TokenType = "CLOSE_CURLY"       // }
+	OPEN_PARENTHESES  TokenType = "OPEN_PARENTHESES"  // (
+	CLOSE_PARENTHESES TokenType = "CLOSE_PARENTHESES" // )
+
+	ASSIGNMENT TokenType = "ASSIGNMENT" // =
+	EQUALS     TokenType = "EQUALS"     // ==
+	NOT        TokenType = "NOT"
+	NOT_EQUALS TokenType = "NOT_EQUALS" // !=
+
+	LESS           TokenType = "LESS"           // <
+	LESS_EQUAL     TokenType = "LESS_EQUAL"     // <=
+	GREATER        TokenType = "GREATER"        // >
+	GREATER_EQUALS TokenType = "GREATER_EQUALS" // >=
+
+	OR  TokenType = "OR"  // ||
+	AND TokenType = "AND" // &&
+
+	NULL  TokenType = "NULL"  // null
+	TRUE  TokenType = "TRUE"  // true
+	FALSE TokenType = "FALSE" // false
+
+	DOT        TokenType = "DOT"        //.
+	DOT_DOT    TokenType = "DOT_DOT"    //..
+	SEMI_COLON TokenType = "SEMI_COLON" // ;
+	COLON      TokenType = "COLON"      // :
+	QUESTION   TokenType = "QUESTION"   //?
+	COMMA      TokenType = "COMMA"      //,
+	WHITESPACE TokenType = "WHITESPACE" // Whitespace
+
+	PLUS_PLUS    TokenType = "PLUS_PLUS"    // ++
+	MINUS_MINUS  TokenType = "MINUS_MINUS"  // --
+	PLUS_EQUALS  TokenType = "PLUS_EQUALS"  // +=
+	MINUS_EQUALS TokenType = "MINUS_EQUALS" // -=
+	SLASH_EQUALS TokenType = "SLASH_EQUALS" // /=
+	STAR_EQUALS  TokenType = "STAR_EQUALS"  // *=
+
+	PLUS    TokenType = "PLUS"    // +
+	DASH    TokenType = "DASH"    // -
+	SLASH   TokenType = "SLASH"   // /
+	STAR    TokenType = "STAR"    // *
+	PERCENT TokenType = "PERCENT" // %
+	BANG    TokenType = "BANG"    //!
+
+	/* ====== RESERVED KEYWORDS ======= */
+	LET        TokenType = "LET"        // let
+	CONST      TokenType = "CONST"      // const
+	CLASS      TokenType = "CLASS"      // class
+	NEW        TokenType = "NEW"        // new
+	IMPORT     TokenType = "IMPORT"     // import
+	FROM       TokenType = "FROM"       // from
+	FN         TokenType = "FN"         // fn
+	IF         TokenType = "IF"         // if
+	ELSE       TokenType = "ELSE"       // else
+	FOREACH    TokenType = "FOREACH"    // foreach
+	WHILE      TokenType = "WHILE"      // while
+	FOR        TokenType = "FOR"        // for
+	EXPORT     TokenType = "EXPORT"     // export
+	TYPEOF     TokenType = "TYPEOF"     // typeof
+	IN         TokenType = "IN"         // in
+	RETURN     TokenType = "RETURN"     // return
+	BREAK      TokenType = "BREAK"      // break
+	CONTINUE   TokenType = "CONTINUE"   // continue
+	DEFER      TokenType = "DEFER"      // defer
+	VAR        TokenType = "VAR"        // var
+	NIL        TokenType = "NIL"        // nil
+	STRUCT     TokenType = "STRUCT"     // struct
+	INTERFACE  TokenType = "INTERFACE"  // interface
+	IMPLEMENTS TokenType = "IMPLEMENTS" // implements
+	EXTENDS    TokenType = "EXTENDS"    // extends
+	PACKAGE    TokenType = "PACKAGE"    // package
+	FUNCTION   TokenType = "FUNCTION"   // function
+
+)
+
+// isAmongDefined checks if the current token type is among the expected token types.
+//
+// Parameters:
+// - expectedTokens: A variadic parameter that accepts any number of TokenType values.
+//
+// Returns:
+//   - A boolean value indicating whether the current token type is among the expected token types.
+//     Returns true if the current token type is found in the expectedTokens slice, otherwise returns false.
+func (token Token) isAmongDefined(expectedTokens ...TokenType) bool {
+	for _, expected := range expectedTokens {
+		if expected == token.Type {
+			return true
+		}
+	}
+	return false
+}
+
+// HELPER METHODS FOR DEBUGGING TOKENS
+/*
+ Inside the Debug method, it checks the Type of the token. If the token kind is IDENTIFIER, NUMBER, or STRING, it prints the token kind followed by its value in parentheses.
+ For example, if the token is an identifier with the value "example", it would print "identifier(example)".
+*/
+func (token Token) Debug() {
+	if token.isAmongDefined(IDENTIFIER, INT, STRING) {
+		fmt.Printf("%s (%s)\n", TokenType(token.Type), token.Literal)
+	} else {
+		fmt.Printf("%s ()\n", TokenType(token.Type))
+	}
+}
+
+// readIdentifier extracts a sequence of characters from the input that form an identifier.
+// It continues reading characters until it encounters a non-identifier character (e.g., whitespace or punctuation).
+//
+// Parameters:
+// - l: A pointer to the Lexer struct. This function is a method of the Lexer struct, so it has access to its fields and methods.
+//
+// Returns:
+// - A string representing the extracted identifier.
+
+func IsLetter(s string) bool {
+	for _, char := range s {
+		if char >= 'a' && char <= 'z' || char >= 'A' && char <= 'Z' {
+			return true
+		}
+	}
+	return false
+}
+
+// readIdentifier extracts a sequence of characters from the input that form an identifier.
+// It continues reading characters until it encounters a non-identifier character (e.g., whitespace or punctuation).
+//
+// Parameters:
+// - l: A pointer to the Lexer struct. This function is a method of the Lexer struct, so it has access to its fields and methods.
+//
+// Returns:
+// - A string representing the extracted identifier.
+func (l *Lexer) readIdentifier() string {
+	startPosition := l.position
+	for IsLetter(string(l.currentChar)) || l.currentChar == '_' {
+		l.getChar()
+	}
+	return l.input[startPosition:l.position]
+}
+
+// Function used to create a lexer
+func NewToken(Type TokenType, value string) Token {
+	return Token{
+		Type, value,
+	}
+}
+
+// Tokenize initializes and returns a new Lexer instance with the given input string.
+// The Lexer is responsible for breaking down the input string into a sequence of tokens.
+//
+// Parameters:
+// - input: A string containing the source code to be tokenized.
+//
+// Returns:
+// - A pointer to a new Lexer instance, initialized with the provided input string.
+func Tokenize(input string) *Lexer {
+	tok := &Lexer{input: input} // Create a new Lexer instance with the given input string
+	tok.getChar()               // Initialize the current character and position of the lexer
+	return tok                  // Return the initialized Lexer instance
+}
+
+// getNextChar advances the lexer to the next character in the input string.
+// It updates the current character (currentChar), the current position (position),
+// and the read position (readPosition). If the read position is at or beyond the end of the input string,
+// the current character is set to 0.
+func (tokens *Lexer) getChar() {
+	if tokens.readPosition >= len(tokens.input) {
+		tokens.currentChar = 0 // ASCII code -> NULL
+	} else {
+		tokens.currentChar = rune(tokens.input[tokens.readPosition])
+	}
+	tokens.position = tokens.readPosition
+	tokens.readPosition++
+}
+
+// peekChar returns the next character in the input string without advancing the lexer's position.
+// If the read position is at or beyond the end of the input string, it returns 0 (ASCII code for NULL).
+//
+// Parameters:
+// - l: A pointer to the Lexer struct. This function is a method of the Lexer struct, so it has access to its fields and methods.
+//
+// Returns:
+// - A rune representing the next character in the input string. If the read position is at or beyond the end of the input string, it returns 0.
+func (l *Lexer) peekChar() rune {
+	if l.readPosition >= len(l.input) {
+		return 0 // End of input
+	}
+	return rune(l.input[l.readPosition])
+}
+
+// skipWhitespace is a method of the Lexer struct that skips over whitespace characters in the input string.
+// It iterates through the characters of the input string, starting from the current character,
+// and advances the lexer's position to the next non-whitespace character.
+//
+// Parameters:
+// - l: A pointer to the Lexer struct. This function is a method of the Lexer struct, so it has access to its fields and methods.
+//
+// Return:
+// This function does not return any value. It modifies the lexer's position and current character.
+func (l *Lexer) skipWhitespace() {
+	for l.currentChar == ' ' || l.currentChar == '\t' || l.currentChar == '\n' || l.currentChar == '\r' {
+		l.getChar()
+	}
+}
+
+// readNumber is a method of the Lexer struct that reads a sequence of digits and an optional decimal point
+// from the input string and returns the corresponding number as a string.
+//
+// The method starts by checking if the current character is a digit. If it is, it appends the digit to the number string
+// and advances the lexer's position to the next character. It then checks if the next character is a decimal point.
+// If it is, it appends the decimal point to the number string and advances the lexer's position to the next character.
+// Finally, it continues reading digits and appending them to the number string until a non-digit character is encountered.
+//
+// Parameters:
+// - l: A pointer to the Lexer struct. This function is a method of the Lexer struct, so it has access to its fields and methods.
+//
+// Return:
+// - A string representing the number read from the input string.
+func (l *Lexer) readNumber() string {
+	var number string
+
+	for IsDigit(string(l.currentChar)) {
+		number += string(l.currentChar)
+		l.getChar() // read & fetch the character
+	}
+
+	if l.currentChar == '.' {
+		number += string(l.currentChar)
+		l.getChar() // read & fetch the character
+
+		for IsDigit(string(l.currentChar)) {
+			number += string(l.currentChar)
+		}
+	}
+	return number
+}
+
+func (l *Lexer) ReadString() string {
+	var str string
+	l.getChar() // Skip the opening quote
+
+	for l.currentChar != '"' && l.currentChar != 0 {
+		str += string(l.currentChar)
+		l.getChar()
+	}
+
+	l.getChar() // Skip the closing quote
+	return str
+}
+
+func IsDigit(s string) bool {
+	for _, char := range s {
+		if char >= '0' && char <= '9' {
+			return true
+		}
+	}
+	return false
+}
+
+func (l *Lexer) GetNextToken() Token {
+	var tok Token
+
+	l.skipWhitespace()
+
+	switch l.currentChar {
+	case '[':
+		tok = newToken(OPEN_BRACKET, string(l.currentChar))
+	case ']':
+		tok = newToken(CLOSE_BRACKET, string(l.currentChar))
+	case '{':
+		tok = newToken(OPEN_CURLY, string(l.currentChar))
+	case '}':
+		tok = newToken(CLOSE_CURLY, string(l.currentChar))
+	case '(':
+		tok = newToken(OPEN_PARENTHESES, string(l.currentChar))
+	case ')':
+		tok = newToken(CLOSE_PARENTHESES, string(l.currentChar))
+	case '=':
+		if l.peekChar() == '=' {
+			l.getChar()
+			tok = newToken(EQUALS, "==")
+		} else {
+			tok = newToken(ASSIGNMENT, string(l.currentChar))
+		}
+	case '+':
+		if l.peekChar() == '+' {
+			l.getChar()
+			tok = newToken(PLUS_PLUS, "++")
+		} else if l.peekChar() == '=' {
+			l.getChar()
+			tok = newToken(PLUS_EQUALS, "+=")
+		} else {
+			tok = newToken(PLUS, string(l.currentChar))
+		}
+	case '-':
+		if l.peekChar() == '-' {
+			l.getChar()
+			tok = newToken(MINUS_MINUS, "--")
+		} else if l.peekChar() == '=' {
+			l.getChar()
+			tok = newToken(MINUS_EQUALS, "-=")
+		} else {
+			tok = newToken(DASH, string(l.currentChar))
+		}
+	case '!':
+		if l.peekChar() == '=' {
+			l.getChar()
+			tok = newToken(NOT_EQUALS, "!=")
+		} else {
+			tok = newToken(NOT, string(l.currentChar))
+		}
+	case '*':
+		if l.peekChar() == '=' {
+			l.getChar()
+			tok = newToken(STAR_EQUALS, "*=")
+		} else {
+			tok = newToken(STAR, string(l.currentChar))
+		}
+	case '/':
+		if l.peekChar() == '=' {
+			l.getChar()
+			tok = newToken(SLASH_EQUALS, "/=")
+		} else {
+			tok = newToken(SLASH, string(l.currentChar))
+		}
+	case '<':
+		if l.peekChar() == '=' {
+			l.getChar()
+			tok = newToken(LESS_EQUAL, "<=")
+		} else {
+			tok = newToken(LESS, string(l.currentChar))
+		}
+	case '>':
+		if l.peekChar() == '=' {
+			l.getChar()
+			tok = newToken(GREATER_EQUALS, ">=")
+		} else {
+			tok = newToken(GREATER, string(l.currentChar))
+		}
+	case '|':
+		if l.peekChar() == '|' {
+			l.getChar()
+			tok = newToken(OR, "||")
+		}
+	case '&':
+		if l.peekChar() == '&' {
+			l.getChar()
+			tok = newToken(AND, "&&")
+		}
+	case '.':
+		if l.peekChar() == '.' {
+			l.getChar()
+			if l.peekChar() == '.' {
+				l.getChar()
+				tok = newToken(DOT_DOT, "...")
+			} else {
+				tok = newToken(DOT, string(l.currentChar))
+			}
+		} else {
+			tok = newToken(DOT, string(l.currentChar))
+		}
+	case ';':
+		tok = newToken(SEMI_COLON, string(l.currentChar))
+	case ':':
+		tok = newToken(COLON, string(l.currentChar))
+	case '?':
+		tok = newToken(QUESTION, string(l.currentChar))
+	case ',':
+		tok = newToken(COMMA, string(l.currentChar))
+	case '0':
+		// Special case to handle the end of input
+		tok = newToken(EOF, "")
+	case ' ', '\t', '\n', '\r':
+		// Handling whitespace
+		tok = newToken(WHITESPACE, string(l.currentChar))
+	case '"':
+		tok = newToken(STRING, l.ReadString())
+	default:
+		if IsLetter(string(l.currentChar)) || l.currentChar == '_' {
+			ident := l.readIdentifier()
+			// tok.Literal = l.readIdentifier()
+			// tok.Type = l.LookupIdentifier(tok.Literal)
+
+			switch ident {
+			case "let":
+				tok = newToken(LET, ident)
+			case "const":
+				tok = newToken(CONST, ident)
+			case "class":
+				tok = newToken(CLASS, ident)
+			case "new":
+				tok = newToken(NEW, ident)
+			case "import":
+				tok = newToken(IMPORT, ident)
+			case "from":
+				tok = newToken(FROM, ident)
+			case "fn":
+				tok = newToken(FN, ident)
+			case "if":
+				tok = newToken(IF, ident)
+			case "else":
+				tok = newToken(ELSE, ident)
+			case "foreach":
+				tok = newToken(FOREACH, ident)
+			case "while":
+				tok = newToken(WHILE, ident)
+			case "for":
+				tok = newToken(FOR, ident)
+			case "export":
+				tok = newToken(EXPORT, ident)
+			case "typeof":
+				tok = newToken(TYPEOF, ident)
+			case "in":
+				tok = newToken(IN, ident)
+			case "return":
+				tok = newToken(RETURN, ident)
+			case "break":
+				tok = newToken(BREAK, ident)
+			case "continue":
+				tok = newToken(CONTINUE, ident)
+			case "defer":
+				tok = newToken(DEFER, ident)
+			case "var":
+				tok = newToken(VAR, ident)
+			case "nil":
+				tok = newToken(NIL, ident)
+			case "struct":
+				tok = newToken(STRUCT, ident)
+			case "interface":
+				tok = newToken(INTERFACE, ident)
+			case "implements":
+				tok = newToken(IMPLEMENTS, ident)
+			case "extends":
+				tok = newToken(EXTENDS, ident)
+			case "package":
+				tok = newToken(PACKAGE, ident)
+			case "function":
+				tok = newToken(FUNCTION, ident)
+			default:
+				tok = newToken(IDENTIFIER, ident)
+			}
+		} else if IsDigit(string(l.currentChar)) {
+			tok = newToken(INT, l.readNumber())
+		} else {
+			tok = newToken(ILLEGAL, string(l.currentChar))
+		}
+	}
+
+	l.getChar()
+	return tok
+}
+
+func newToken(tokenType TokenType, currentChar string) Token {
+	return Token{
+		Type:    tokenType,
+		Literal: currentChar,
+	}
+}
+
+// func (l *Lexer) LookupIdent() TokenType {
+// 	switch  {
+// 	case "let":
+// 		newToken(LET, string(l.currentChar))
+// 	case "const":
+// 		newToken(CONST, ident)
+// 	case "class":
+// 		newToken(CLASS, ident)
+// 	case "new":
+// 		newToken(NEW, ident)
+// 	case "import":
+// 		newToken(IMPORT, ident)
+// 	case "from":
+// 		newToken(FROM, ident)
+// 	case "fn":
+// 		newToken(FN, ident)
+// 	case "if":
+// 		newToken(IF, ident)
+// 	case "else":
+// 		newToken(ELSE, ident)
+// 	case "foreach":
+// 		newToken(FOREACH, ident)
+// 	case "while":
+// 		newToken(WHILE, ident)
+// 	case "for":
+// 		newToken(FOR, ident)
+// 	case "export":
+// 		newToken(EXPORT, ident)
+// 	case "typeof":
+// 		newToken(TYPEOF, ident)
+// 	case "in":
+// 		newToken(IN, ident)
+// 	case "return":
+// 		newToken(RETURN, ident)
+// 	case "break":
+// 		newToken(BREAK, ident)
+// 	case "continue":
+// 		newToken(CONTINUE, ident)
+// 	case "defer":
+// 		newToken(DEFER, ident)
+// 	case "var":
+// 		newToken(VAR, ident)
+// 	case "nil":
+// 		newToken(NIL, ident)
+// 	case "struct":
+// 		newToken(STRUCT, ident)
+// 	case "interface":
+// 		newToken(INTERFACE, ident)
+// 	case "implements":
+// 		newToken(IMPLEMENTS, ident)
+// 	case "extends":
+// 		newToken(EXTENDS, ident)
+// 	case "package":
+// 		newToken(PACKAGE, ident)
+// 	case "function":
+// 		newToken(FUNCTION, ident)
+// 	default:
+// 		newToken(IDENTIFIER, ident)
+// 	}
+// 	return IDENTIFIER
+// }
